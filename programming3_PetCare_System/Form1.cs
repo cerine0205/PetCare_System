bdf71f1e7053acf7ef98e55d4732b0ea5c18239e
Vanessa <hsma2337@gmail.com>
2025-11-09 11:48:40 +0300

    add label

--- programming3_PetCare_System/Form1.cs
+++ programming3_PetCare_System/Form1.cs
@@ -16,5 +16,10 @@ namespace programming3_PetCare_System
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
